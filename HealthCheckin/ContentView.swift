0dcaaedacce0aa72feaf1bbf1bb16b82ab9c31d9
Joaquin Campo <jcampo@pento.ai>
2025-09-07 21:07:44 -0300

    Initial Commit

---
+++ HealthCheckin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HealthCheckin
+//
+//  Created by Joaquin Campo Nario on 7/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HealthCheckin/HealthCheckinApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthCheckinApp.swift
+//  HealthCheckin
+//
+//  Created by Joaquin Campo Nario on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthCheckinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
